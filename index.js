34e69263dd7874d523f8b9396541e9eefc13aeae
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 04:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,8 +49,14 @@ function removeFromCart(item) {
       return cart;
     }
   }
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber == null) {
+    System.out.println('Sorry, we don\'t have a credit card on file for you.');
+  }
+  let total = total();
+  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
 }
