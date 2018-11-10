6ba01f5d98ee94575a9ed1f5ccda383fe70e6eea
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 04:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName == item) {
+      cart.splice(i, 1);
+      return
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
