bc003b0f3b561b8b0b5f25d70989f0858963ecf5
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 04:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber == null) {
+  if (cardNumber === null) {
     System.out.println('Sorry, we don\'t have a credit card on file for you.');
   }
   cart = [];
